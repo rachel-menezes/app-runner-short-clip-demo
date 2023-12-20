5b4682951f0388f9dabdf15781e8ff661040d121
runlaiyang <yangrunlai@vip.qq.com>
2023-12-19 18:10:51 -0800

    Update mem.js

--- mem.js
+++ mem.js
@@ -1,2 +1,9 @@
-var buffer = Buffer.alloc(5* 1024 * 1024 * 1024, 'a');
-console.log("Done")
+var buffer = Buffer.alloc(1* 1024 * 1024 * 1024, 'a');
+console.log("A")
+console.log(buffer)
+var bufferB = Buffer.alloc(2* 1024 * 1024 * 1024, 'b');
+console.log("B")
+console.log(bufferB)
+var bufferC = Buffer.alloc(1* 1024 * 1024 * 1024, 'c');
+console.log("C")
+console.log(bufferC)
